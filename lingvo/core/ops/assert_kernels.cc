b82b22aeba5cfa721acd35eaffcda5e70a9f384d
anjaligopi <anjaligopi23@gmail.com>
2020-09-23 06:10:05 +0000

    revert bucket_batch_limit

--- lingvo/core/ops/assert_kernels.cc
+++ lingvo/core/ops/assert_kernels.cc
@@ -103,8 +103,10 @@ class AssertSameDim0Op : public OpKernel {
 REGISTER_KERNEL_BUILDER(Name("AssertSameDim0").Device(DEVICE_CPU),
                         AssertSameDim0Op);
 
+// #if GOOGLE_CUDA
 REGISTER_KERNEL_BUILDER(Name("AssertSameDim0").Device(DEVICE_GPU),
                         AssertSameDim0Op);
+// #endif
 
 }  // namespace
 }  // namespace lingvo
