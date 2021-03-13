148d4b801475ae685b47206f228fe097d4c016bd
Daniel Galvez <dt.galvez@gmail.com>
2021-03-13 18:57:26 +0000

    Some missing things.

---
+++ scripts/archive.org/find_license_url_values.py
@@ -0,0 +1,116 @@
+# Output from this script:
+# (1, 'Government Access TV')
+# (1, 'Massachusetts')
+# (1, 'México')
+# (1, 'Wakefield')
+# (1, 'http://creativecommons.org/licenses/by-nc-nd/2.5/es/')
+# (1, 'http://creativecommons.org/licenses/by-nc-nd/3.0/es/')
+# (1, 'http://creativecommons.org/licenses/by-nc-sa/2.0/uk/')
+# (1, 'http://creativecommons.org/licenses/by-nc-sa/2.5/br/')
+# (1, 'http://creativecommons.org/licenses/by-nc-sa/2.5/mx/')
+# (1, 'http://creativecommons.org/licenses/by-nc-sa/2.5/se/')
+# (1, 'http://creativecommons.org/licenses/by-nc-sa/3.0/es/')
+# (1, 'http://creativecommons.org/licenses/by-nc-sa/3.0/hk/')
+# (1, 'http://creativecommons.org/licenses/by-nc/2.0/')
+# (1, 'http://creativecommons.org/licenses/by-nc/2.0/de/')
+# (1, 'http://creativecommons.org/licenses/by-nc/2.0/uk/')
+# (1, 'http://creativecommons.org/licenses/by-nd-nc/1.0/')
+# (1, 'http://creativecommons.org/licenses/by-nd/2.5/')
+# (1, 'http://creativecommons.org/licenses/by-sa/2.5/')
+# (1, 'http://creativecommons.org/licenses/by-sa/2.5/mx/')
+# (1, 'http://creativecommons.org/licenses/by-sa/3.0/br/')
+# (1, 'http://creativecommons.org/licenses/by-sa/3.0/it/')
+# (1, 'http://creativecommons.org/licenses/by/2.5/')
+# (1, 'http://creativecommons.org/licenses/by/2.5/ar/')
+# (1, 'http://creativecommons.org/licenses/by/2.5/it/')
+# (1, 'http://creativecommons.org/licenses/by/2.5/pl/')
+# (1, 'http://www.copyright.gov/')
+# (1, 'https://creativecommons.org/publicdomain/zero/1.0/')
+# (2, 'http://creativecommons.org/licenses/by-nc-nd/2.0/')
+# (2, 'http://creativecommons.org/licenses/by-nc-nd/2.5/ar/')
+# (2, 'http://creativecommons.org/licenses/publicdomain/prelinger/')
+# (3, 'http://creativecommons.org/licenses/by-nc-sa/3.0/it/')
+# (3, 'http://creativecommons.org/licenses/by-nc/2.5/')
+# (4, 'http://creativecommons.org/licenses/by-sa/3.0/de/')
+# (5, 'http://creativecommons.org/licenses/by-nd/3.0/us/')
+# (6, 'http://creativecommons.org/licenses/by-sa/3.0/es/')
+# (7, 'http://creativecommons.org/licenses/by-nc-sa/2.5/')
+# (9, 'http://creativecommons.org/licenses/by/3.0/us/')
+# (10, 'http://creativecommons.org/licenses/by-nc-sa/2.0/')
+# (11, 'http://creativecommons.org/licenses/by-sa/3.0/us/')
+# (12, 'http://creativecommons.org/licenses/by-nc/3.0/us/')
+# (13, 'https://creativecommons.org/licenses/by-sa/4.0')
+# (16, 'http://creativecommons.org/licenses/by-nc-nd/3.0/n')
+# (26, 'Attribution-NonCommercial-ShareAlike 3.0 Generic')
+# (34, 'http://creativecommons.org/licenses/by/4.0/')
+# (34, 'https://creativecommons.org/licenses/by/4.0/')
+# (39, 'http://creativecommons.org/licenses/by-nd/2.0/')
+# (84, 'http://creativecommons.org/licenses/by-nc-sa/3.0')
+# (93, 'http://creativecommons.org/licenses/publicdomain')
+# (102, 'http://creativecommons.org/licenses/by-nc-nd/2.5/')
+# (193, 'https://creativecommons.org/licenses/by-nd/4.0/')
+# (228, 'https://creativecommons.org/licenses/cc0/3.0/')
+# (302, 'https://creativecommons.org/licenses/by-nc/4.0/')
+# (304, 'http://creativecommons.org/licenses/by/2.0/')
+# (332, 'http://creativecommons.org/licenses/by-sa/4.0/')
+# (386, 'http://creativecommons.org/licenses/by-nc/4.0/')
+# (395, 'http://creativecommons.org/licenses/by-nd/4.0/')
+# (462, 'http://creativecommons.org/licenses/by-nc-sa/3.0/us/')
+# (471, 'https://creativecommons.org/licenses/by/3.0')
+# (644, 'http://creativecommons.org/licenses/by/3.0/')
+# (1033, 'http://creativecommons.org/licenses/by-sa/3.0/')
+# (1208, 'https://creativecommons.org/licenses/by-nc-nd/3.0/deed.en_GB')
+# (1331, 'http://creativecommons.org/licenses/by-nc-nd/3.0/us/')
+# (1462, 'https://creativecommons.org/licenses/by-nc-sa/4.0/')
+# (1686, 'http://creativecommons.org/publicdomain/zero/1.0/')
+# (1733, 'https://creativecommons.org/licenses/by-sa/3.0/')
+# (1749, 'https://creativecommons.org/licenses/by-nd/3.0/')
+# (1885, 'https://creativecommons.org/licenses/by-nc-nd/4.0/')
+# (1942, 'http://creativecommons.org/licenses/by-nd/3.0/')
+# (2259, 'http://creativecommons.org/licenses/by-nc-sa/3.0/')
+# (2500, 'http://creativecommons.org/licenses/by-nc/3.0/')
+# (2877, 'http://creativecommons.org/licenses/by-nc-nd/4.0/')
+# (3898, 'https://creativecommons.org/licenses/by/3.0/')
+# (4215, 'http://creativecommons.org/publicdomain/mark/1.0/')
+# (5355, 'http://creativecommons.org/licenses/publicdomain/')
+# (8002, 'https://creativecommons.org/licenses/by-sa/4.0/')
+# (8733, 'https://creativecommons.org/licenses/by-nc/3.0/')
+# (9456, 'https://creativecommons.org/licenses/by-nc-sa/3.0/')
+# (13230, 'https://creativecommons.org/licenses/by-nc-nd/3.0/')
+# (19547, 'http://creativecommons.org/licenses/by-nc-sa/4.0/')
+# (23320, 'http://creativecommons.org/licenses/by-nc-nd/3.0/')
+# (46546, 'https://www.usa.gov/government-works')
+#  17956
+
+from collections import defaultdict
+
+from internetarchive.session import ArchiveSession
+from internetarchive.search import Search
+
+from tqdm import tqdm
+
+def grab_key_values(key):
+  s = ArchiveSession()
+  search = Search(s, '(mediatype:audio OR mediatype:movies) AND (closed_captioning:yes OR format:SubRip OR format:"Web Video Text Tracks")',
+                  fields=[key])
+  licenses = defaultdict(int)
+  for result in tqdm(search):
+    if key not in result:
+      print(f"No {key} result:", result)
+      continue
+    if isinstance(result[key], str):
+      licenses[result[key]] += 1
+    elif isinstance(result[key], list):
+      for licenseurl in result[key]:
+        licenses[licenseurl] += 1
+      else:
+        raise ValueError(f"Unexpected type for {key}: ", type(result[key]), result)
+
+  print(f"Counts for key={key}")
+  print("\n".join(str(x) for x in sorted((v, k) for k, v in licenses.items())))
+
+#grab_key_values("lcenseurl")
+#grab_key_values("license")
+#grab_key_values("licenseurl")
+#grab_key_values("rights")
+grab_key_values("possible-copyright-status")
