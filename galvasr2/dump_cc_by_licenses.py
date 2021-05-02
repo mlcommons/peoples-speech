9b140eee89d97d63eb04a4ef5bc55d476254f9cb
ciroye <jmcirot@unal.edu.co>
2021-05-02 21:50:45 +0000

    create flag in the function cc_by,add documentation and add .ipynb_checkpoints to .gitignore

--- galvasr2/dump_cc_by_licenses.py
+++ galvasr2/dump_cc_by_licenses.py
@@ -2,8 +2,29 @@
 
 from align.spark.schemas import ARCHIVE_ORG_SCHEMA
 from pyspark.sql import SparkSession
+from absl import flags
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string('input_catalogue_path',
+                   'gs://the-peoples-speech-west-europe/archive_org/Mar_7_2021/EXPANDED_LICENSES_FILTERED_ACCESS.jsonl.gz',
+                   'Ubication of the path with the licence metadata')
+flags.DEFINE_string('save_as',
+                   'csv',
+                   'Format to save the file')
 
 def myConcat(*cols):
+    """Generate a format that allows import a Spark df as a one column txt
+    
+        Parameters
+        ----------
+        *cols : list
+            columns
+        Returns
+        -------
+        Spark df
+            Data in the format needed to save as a txt
+    """
     concat_columns = []
     for c in cols[:-1]:
         concat_columns.append(F.coalesce(c, F.lit("*")))
@@ -12,6 +33,21 @@ def myConcat(*cols):
     return F.concat(*concat_columns)
 
 def create_dump_license(spark:SparkSession, input_catalogue_path:str="gs://the-peoples-speech-west-europe/archive_org/Mar_7_2021/EXPANDED_LICENSES_FILTERED_ACCESS.jsonl.gz", save_as:str='csv'):
+    """Function that takes the type of licenses to verify in which one needs to grant the necessary credits and deliver the file (cvs, txt, etc) with this data
+    
+        Parameters
+        ----------
+        spark : SparkSession
+            Necessary SparkSession
+        input_catalogue_path : str
+            Ubication of the path
+        save_as : str
+            Format to save the file
+        Returns
+        -------
+        Str
+            Status of the file generation 
+    """
     df = spark.read.format('json').schema(ARCHIVE_ORG_SCHEMA).load(input_catalogue_path)
     ##Filter by necessary columns 
     columns = [df.metadata.licenseurl, df.metadata.creator, df.metadata.title, df.metadata.credits]
@@ -29,11 +65,13 @@ def create_dump_license(spark:SparkSession, input_catalogue_path:str="gs://the-p
     elif save_as == 'txt':
         df = df.withColumn("credits", myConcat(*df.columns)).select("credits")
         df.coalesce(1).write.format("text").option("header", "false").mode("append").save("credits.txt")
+    else:
+        return 'This format to save is not allowed'
     return 'save file successful'
 
 def main():
     spark = SparkSession.builder.appName('CC-BY-license').getOrCreate()
-    create_dump_license(spark)
+    create_dump_license(spark, FLAGS.input_catalogue_path, FLAGS.save_as)
 
 if __name__ == '__main__':
     main()
