a65560c24b66b36b7319571a33c749bbead1710a
greg-landing <greg@landing.ai>
2021-05-17 17:47:48 -0700

    ENH: Cloudbuild wwork.

--- platform/micro-services/data-export/build-scripts/flask/docker/build-container.sh
+++ platform/micro-services/data-export/build-scripts/flask/docker/build-container.sh
@@ -10,5 +10,5 @@ set -euf -o pipefail
 # Get the directory of this script
 LOCAL_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-docker build -t peoples-speech-data-export-worker:0.1 -f $LOCAL_DIRECTORY/Dockerfile $LOCAL_DIRECTORY/../..
+docker build -t data-export:0.1 -f $LOCAL_DIRECTORY/Dockerfile $LOCAL_DIRECTORY/../../..
 
