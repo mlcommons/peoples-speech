34d8a11b47d102d79034c2c0c610b5e4a0018da7
JFCeron <juanfceron@gmail.com>
2021-10-11 10:51:01 -0500

    Train with seed

---
+++ training/nemo/train_ctc_model.py
@@ -0,0 +1,38 @@
+import pytorch_lightning as pl
+from omegaconf import OmegaConf
+
+from nemo.collections.asr.models import EncDecCTCModel
+from nemo.core.config import hydra_runner
+from nemo.utils import logging
+from nemo.utils.exp_manager import exp_manager
+
+SEED = 41
+
+@hydra_runner(config_path="conf", config_name="config")
+def main(cfg):
+    logging.info(f'Hydra config: {OmegaConf.to_yaml(cfg)}')
+
+    pl.utilities.seed.seed_everything(SEED)
+
+    trainer = pl.Trainer(**cfg.trainer)
+    exp_manager(trainer, cfg.get("exp_manager", None))
+    asr_model = EncDecCTCModel(cfg=cfg.model, trainer=trainer)
+
+    # Initialize the weights of the model from another model, if provided via config
+    asr_model.maybe_init_from_pretrained_checkpoint(cfg)
+
+    trainer.fit(asr_model)
+
+    if hasattr(cfg.model, 'test_ds') and cfg.model.test_ds.manifest_filepath is not None:
+        gpu = 1 if cfg.trainer.gpus != 0 else 0
+        test_trainer = pl.Trainer(
+            gpus=gpu,
+            precision=trainer.precision,
+            amp_level=trainer.accelerator_connector.amp_level,
+            amp_backend=cfg.trainer.get("amp_backend", "native"),
+        )
+        if asr_model.prepare_test(test_trainer):
+            test_trainer.test(asr_model)
+
+if __name__ == '__main__':
+    main()  # noqa pylint: disable=no-value-for-parameter
