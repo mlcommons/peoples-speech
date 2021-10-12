724e187b03ad99ad9eb9300d5b80ec16a5e20e30
JFCeron <juanfceron@gmail.com>
2021-10-11 19:53:02 -0500

    Continue training after shutdown

--- training/nemo/train-tpspeech.sh
+++ training/nemo/train-tpspeech.sh
@@ -2,26 +2,33 @@ gpu_count=$1
 manifest_path=$2
 tarred_audio_paths=$3
 val_manifest_path=$4
+exp_name=$5
+init_ckpt_path=${6-null}
+init_ckpt_path=${init_ckpt_path//=/\\=}
+
 echo "GPU count: $gpu_count"
 echo "Manifest filepath: $manifest_path"
-echo "Tarred audio paths: $tarred_audio_path"
+echo "Tarred audio paths: $tarred_audio_paths"
 echo "LibriSpeech dev clean manifest: $val_manifest_path"
+echo "Experiment name: $exp_name"
+echo "Load weights and schedule from : $init_ckpt_path"
 
 echo "---------------"
 echo "Starting training"
 export CUDA_VISIBLE_DEVICES=$(seq -s, 0 1 $gpu_count)
-echo $CUDA_VISIBLE_DEVICES
 cd "${0%/*}"
-cd NeMo
-python -m examples.asr.speech_to_text \
-    --config-path=conf/quartznet/ \
+python train_ctc_model.py \
+    --config-path=NeMo/examples/asr/conf/quartznet/ \
     --config-name=quartznet_15x5 \
-    model.train_ds.manifest_filepath=$2 \
-    model.train_ds.tarred_audio_filepaths=$3 \
+    +trainer.resume_from_checkpoint=${init_ckpt_path} \
+    exp_manager.exp_dir=trained-models \
+    exp_manager.name=${exp_name} \
+    model.train_ds.manifest_filepath=${manifest_path} \
+    model.train_ds.tarred_audio_filepaths=${tarred_audio_paths} \
     model.train_ds.is_tarred=true \
     model.train_ds.batch_size=$((32 / $gpu_count)) \
     +model.train_ds.num_workers=8 \
-    model.validation_ds.manifest_filepath=$4 \
+    model.validation_ds.manifest_filepath=${val_manifest_path} \
     +model.validation_ds.num_workers=4 \
     model.validation_ds.batch_size=2 \
     +model.use_cer=true \
