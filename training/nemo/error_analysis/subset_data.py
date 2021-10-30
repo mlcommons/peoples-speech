1bfaa7d843e0f664e16bbdbc308f7fa40ac7e10c
JFCeron <50839826+JFCeron@users.noreply.github.com>
2021-10-30 15:00:21 -0500

    Nemo training (#52)
    * Add NeMo submodule
    
    * Update NeMo with small spectrogram bug fix
    
    * QuartzNet15x5 training script
    
    * Script to subset a NeMo-style dataset
    
    * Script to transcribe a NeMo dataset using an external language model
    
    * Change transcription key to 'pred_text'
    
    * Script to correct the durations in a manifest
    
    * Train with seed
    
    * Continue training after shutdown
    
    * Transcribe on GPU, with or without external LM
    
    * Store transcripts, metrics, and args in single dir
    
    * Transcribe from tar files
    
    * Transcribe using multiple GPUs
    
    * Fix bug in untarred transcription
    
    * Fix bug involving long file names in tar files
    
    * Take random seed from user
    
    * Fix seed command line argument index
    
    * Remove training .sh script
    
    * Train char-level Conformer
    
    * Grid search over beam search alpha and beta
    
    * Decrease model.optim.sched.min_lr
    
    * Subword-level conformer
    
    * Remove weight decay and augmentation
    
    * Change evaluation metric to WER
    
    * Add support for BPE models
    
    * Debug BPE model training
    
    * Reduce min_lr

---
+++ training/nemo/error_analysis/subset_data.py
@@ -0,0 +1,82 @@
+import os
+import glob
+import json
+import math
+import random
+import tarfile
+import argparse
+from tqdm import tqdm
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Subset a tarred local dataset"
+    )
+    parser.add_argument(
+        "--dataset_tarfiles",
+        required=True,
+        type=str,
+        help="A regex of local .tar files"
+    )
+    parser.add_argument(
+        "--dataset_manifest",
+        required=True,
+        type=str,
+        help="NeMo-style manifest of the large dataset"
+    )
+    parser.add_argument(
+        "--subset_rel_size",
+        required=True,
+        type=float,
+        help="Subset size relative to the large dataset's (e.g. 0.1)"
+    )
+    parser.add_argument(
+        "--tarfiles_frac",
+        required=True,
+        type=float,
+        help="Fraction of all tarfiles to browse while subsetting"
+    )
+    parser.add_argument(
+        "--output_dir",
+        required=True,
+        type=str,
+        help="Name of the output tarfile"
+    )
+    args = parser.parse_args()
+    assert args.tarfiles_frac >= args.subset_rel_size
+    # Build subset tarfile
+    print("Building subset tarfile")
+    os.makedirs(args.output_dir)
+    output_tar_path = os.path.join(args.output_dir, "subset.tar")
+    input_tar_paths = glob.glob(args.dataset_tarfiles)
+    total_tarfiles = len(input_tar_paths)
+    input_tar_paths = random.sample(
+        input_tar_paths,
+        math.ceil(args.tarfiles_frac * total_tarfiles)
+    )
+    real_tarfiles_frac = len(input_tar_paths) / total_tarfiles
+    keeper_names = set()
+    with tarfile.open(output_tar_path, "w") as output:
+        for input_tar_path in tqdm(input_tar_paths):
+            with tarfile.open(input_tar_path, "r:") as input_tar_file:
+                members = input_tar_file.getmembers()
+                n_keepers = len(members) * args.subset_rel_size / real_tarfiles_frac
+                if n_keepers < 1:
+                    n_keepers = 1 if (random.random() < n_keepers) else 0
+                else:
+                    n_keepers = int(n_keepers)
+                keepers = random.sample(members, n_keepers)
+                for keeper in keepers:
+                    output.addfile(keeper, input_tar_file.extractfile(keeper.name))
+                    keeper_names.add(keeper.name)
+    # Build subset manifest
+    print("Building subset manifest")
+    subset_manifest_path = os.path.join(args.output_dir, "manifest.jsonl")
+    with open(args.dataset_manifest, "r") as full_manifest, \
+         open(subset_manifest_path, "w") as subset_manifest:
+        for str_sample in tqdm(full_manifest):
+            sample = json.loads(str_sample)
+            if sample["audio_filepath"] in keeper_names:
+                subset_manifest.write(str_sample)
+
+if __name__ == "__main__":
+    main()
---
+++ training/nemo/train_ctc_model.py
@@ -0,0 +1,40 @@
+import pytorch_lightning as pl
+from omegaconf import OmegaConf
+
+from nemo.collections.asr.models import EncDecCTCModel, EncDecCTCModelBPE
+from nemo.core.config import hydra_runner
+from nemo.utils import logging
+from nemo.utils.exp_manager import exp_manager
+
+@hydra_runner(config_path=".")
+def main(cfg):
+    cfg.model.train_ds.batch_size = cfg.model.train_ds.batch_size // cfg.n_gpus
+    logging.info(f'Hydra config: {OmegaConf.to_yaml(cfg, resolve=True)}')
+
+    pl.utilities.seed.seed_everything(cfg.seed)
+
+    trainer = pl.Trainer(**cfg.trainer)
+    exp_manager(trainer, cfg.get("exp_manager", None))
+    if "tokenizer" in cfg.model:
+        asr_model = EncDecCTCModelBPE(cfg=cfg.model, trainer=trainer)
+    else:
+        asr_model = EncDecCTCModel(cfg=cfg.model, trainer=trainer)
+
+    # Initialize the weights of the model from another model, if provided via config
+    asr_model.maybe_init_from_pretrained_checkpoint(cfg)
+
+    trainer.fit(asr_model)
+
+    if hasattr(cfg.model, 'test_ds') and cfg.model.test_ds.manifest_filepath is not None:
+        gpu = 1 if cfg.trainer.gpus != 0 else 0
+        test_trainer = pl.Trainer(
+            gpus=gpu,
+            precision=trainer.precision,
+            amp_level=trainer.accelerator_connector.amp_level,
+            amp_backend=cfg.trainer.get("amp_backend", "native"),
+        )
+        if asr_model.prepare_test(test_trainer):
+            test_trainer.test(asr_model)
+
+if __name__ == '__main__':
+    main()  # noqa pylint: disable=no-value-for-parameter
