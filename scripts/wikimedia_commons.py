43f3aa32b6ff73fef213abdd8acec06058932f9a
autoblack <jeussej@gmail.com>
2022-11-14 20:08:54 +0000

    fixup: Format Python code with Black

--- scripts/wikimedia_commons.py
+++ scripts/wikimedia_commons.py
@@ -6,7 +6,7 @@ import os
 from pyWikiCommons import pyWikiCommons
 
 
-def get_dataset(xml_file ,output_file_name):
+def get_dataset(xml_file, output_file_name):
     dump = mwxml.Dump.from_file(open(xml_file))
     print(dump.site_info.name, dump.site_info.dbname)
 
@@ -14,64 +14,70 @@ def get_dataset(xml_file ,output_file_name):
     counter = 0
     for page in tqdm(dump):
         if page.namespace == 102:
-            counter+=1
+            counter += 1
             for revision in page:
                 revision = revision
-            pages.append(revision)        
+            pages.append(revision)
             print(counter)
 
     wikimedia = [i.to_json() for i in pages]
 
     import json
-    with open(output_file_name , 'w') as file:
+
+    with open(output_file_name, "w") as file:
         json.dump(wikimedia, file)
 
 
 # Esto solo sirve para .webm
 def change_format(file_name):
     import subprocess
+
     changed_filename = os.rename(file_name, "temp.webm")
     print(true_link)
-    subprocess.run(f'ffmpeg -i "temp.webm" {file_name}.mp3',shell=True)
-    
-def download_file(url,file_name):
-    headers = {'User-Agent': 'CoolBot/0.0 (https://example.org/coolbot/; coolbot@example.org)'}
+    subprocess.run(f'ffmpeg -i "temp.webm" {file_name}.mp3', shell=True)
+
+
+def download_file(url, file_name):
+    headers = {
+        "User-Agent": "CoolBot/0.0 (https://example.org/coolbot/; coolbot@example.org)"
+    }
     with requests.get(url, stream=True, headers=headers) as r:
         r.raise_for_status()
-        with open(file_name, 'wb') as f:
-            for chunk in r.iter_content(chunk_size=8192): 
+        with open(file_name, "wb") as f:
+            for chunk in r.iter_content(chunk_size=8192):
                 f.write(chunk)
     return file_name
 
+
 def split_with_last_dot(string):
-    for character in range(len(string)-1,0,-1):
-        if string[character]==".":
+    for character in range(len(string) - 1, 0, -1):
+        if string[character] == ".":
             left = string[:character]
-            right = string[character+1:]
-            return [left,right] 
+            right = string[character + 1 :]
+            return [left, right]
 
 
 # Download, convert and upload files to GCP
 
 import json
-with open("wikimedia.json" , 'r') as file:
-        previous_json = json.load(file)
+
+with open("wikimedia.json", "r") as file:
+    previous_json = json.load(file)
 
 counter = 0
 database = {}
-#for i in range(len(previous_json)):
+# for i in range(len(previous_json)):
 for i in range(1):
     link = previous_json[i]["page"]["title"]
     dots = 0
-    left,_ = split_with_last_dot(link)
+    left, _ = split_with_last_dot(link)
     true_link, _ = split_with_last_dot(link)
-    
-    
-    with open(link+".txt", 'w') as f:
+
+    with open(link + ".txt", "w") as f:
         f.write(previous_json[i]["text"])
-    
-#    upload_and_delete_txt()
-    
+
+    #    upload_and_delete_txt()
+
     if true_link in database.keys():
         database[true_link].append(link)
     else:
@@ -80,8 +86,8 @@ for i in range(1):
             url = pyWikiCommons.get_commons_url("File:" + true_link)
             download_file(url, true_link)
             change_format(true_link)
-            
-#            upload_and_delete_video()
+
+            #            upload_and_delete_video()
 
             f = open("uploaded.txt", "a")
             f.write(f"{i},{true_link}\n")
@@ -90,4 +96,3 @@ for i in range(1):
             f = open("error.txt", "a")
             f.write(f"{i},{true_link}\n")
             f.close()
-            
